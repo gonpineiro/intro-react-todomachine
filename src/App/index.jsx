fc72ccfbde04a7687104ed41ba4a0ef9244feb3a
Gonzalo <gonzalo@estadodigital.com>
2021-12-23 16:56:23 -0300

    Composicion de Componentes

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppUI } from './AppUI';
+/* import { AppUI } from './AppUI'; */
 import { TodoProvider } from '../TodoContext/';
 // import './App.css';
 
@@ -11,11 +11,47 @@ import { TodoProvider } from '../TodoContext/';
 ]; */
 
 function App() {
+    const [state, setState] = React.useState('estado Compartido');
+
+    return (
+        <>
+            <TodoHeader>
+                <TodoCounter />
+                <TodoSearch />
+            </TodoHeader>
+
+            <TodoList>
+                <TodoItem state={state}/>
+            </TodoList>
+        </>
+    );
+}
+
+function TodoHeader({ children }) {
+    return <header>{children}</header>;
+}
+
+function TodoList({ children }) {
+    return <section className="TodoList-container">{children}</section>;
+}
+
+function TodoCounter() {
+    return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+    return <p>TodoSearch</p>;
+}
+
+function TodoItem({state}) {
+    return <p>TodoItem: {state}</p>;
+}
+/* function App() {
     return (
         <TodoProvider>
             <AppUI />
         </TodoProvider>
     );
-}
+} */
 
 export default App;
