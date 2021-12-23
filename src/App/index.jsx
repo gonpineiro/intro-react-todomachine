a65ab28ed6d14cccd600840998addc2e7c87d0fd
Gonzalo <gonzalo@estadodigital.com>
2021-12-23 10:04:32 -0300

    Refactor

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+import {AppUI} from './AppUI'
 // import './App.css';
 
 const defaultTodos = [
@@ -51,24 +47,15 @@ function App() {
     };
 
     return (
-        <React.Fragment>
-            <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
-            <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-            <TodoList>
-                {searchedTodos.map((todo) => (
-                    <TodoItem
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodos(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                    />
-                ))}
-            </TodoList>
-
-            <CreateTodoButton />
-        </React.Fragment>
+        <AppUI
+            completedTodos={completedTodos}
+            totalTodos={totalTodos}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            searchedTodos={searchedTodos}
+            completeTodos={completeTodos}
+            deleteTodo={deleteTodo}
+        />
     );
 }
 
