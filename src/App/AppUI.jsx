66222188028ae9167fc80cd76dee4abe99102479
Gonzalo <gonzalo@estadodigital.com>
2021-12-23 12:19:21 -0300

    useContext

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 
+import { TodoContext } from '../TodoContext';
+
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
-function AppUI({loading, error, completedTodos, totalTodos, searchValue, setSearchValue, searchedTodos, completeTodos, deleteTodo }) {
+function AppUI() {
     return (
         <React.Fragment>
-            <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
-            <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+            <TodoCounter />
+            <TodoSearch />
 
-            <TodoList>
-                {error && <p>Hubo un error</p>}
-                {loading && <p>Estamos cargando...</p>}
-                {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO...</p>}
+            <TodoContext.Consumer>
+                {({ error, loading, searchedTodos, completeTodos, deleteTodo }) => (
+                    <TodoList>
+                        {error && <p>Hubo un error</p>}
+                        {loading && <p>Estamos cargando...</p>}
+                        {!loading && !searchedTodos.length && <p>Crea tu primer TODO...</p>}
 
-                {searchedTodos.map((todo) => (
-                    <TodoItem
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodos(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                    />
-                ))}
-            </TodoList>
+                        {searchedTodos.map((todo) => (
+                            <TodoItem
+                                key={todo.text}
+                                text={todo.text}
+                                completed={todo.completed}
+                                onComplete={() => completeTodos(todo.text)}
+                                onDelete={() => deleteTodo(todo.text)}
+                            />
+                        ))}
+                    </TodoList>
+                )}
+            </TodoContext.Consumer>
 
             <CreateTodoButton />
         </React.Fragment>
--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppUI } from './AppUI';
+import { TodoProvider } from '../TodoContext/';
 // import './App.css';
 
 /* const defaultTodos = [
@@ -9,94 +10,11 @@ import { AppUI } from './AppUI';
     { text: 'LALALALAA', completed: false },
 ]; */
 
-function useLocalStorage(itemName, initialValue) {
-    const [error, setError] = React.useState(false);
-    const [loading, setLoading] = React.useState(true);
-    const [item, setItem] = React.useState(initialValue);
-
-    React.useEffect(() => {
-        setTimeout(() => {
-            try {
-                const localStorageItem = localStorage.getItem(itemName);
-                let parsedItem;
-
-                if (!localStorageItem) {
-                    localStorage.setItem(itemName, JSON.stringify(initialValue));
-                    parsedItem = initialValue;
-                } else {
-                    parsedItem = JSON.parse(localStorageItem);
-                }
-
-                setItem(parsedItem);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-        }, 1300);
-    });
-
-    const saveItem = (newTodos) => {
-        try {
-            const stringifiedItem = JSON.stringify(newTodos);
-            localStorage.setItem(itemName, stringifiedItem);
-            setItem(newTodos);
-        } catch (error) {
-            setError(error);
-        }
-    };
-
-    return { item, saveItem, loading, error };
-}
-
 function App() {
-    const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
-    const [searchValue, setSearchValue] = React.useState('');
-
-    const completedTodos = todos.filter((todo) => !!todo.completed).length;
-    const totalTodos = todos.length;
-
-    let searchedTodos = [];
-    if (!searchValue.length >= 1) {
-        searchedTodos = todos;
-    } else {
-        searchedTodos = todos.filter((todo) => {
-            const todoText = todo.text.toLowerCase();
-            const searchText = searchValue.toLowerCase();
-
-            return todoText.includes(searchText);
-        });
-    }
-
-    const completeTodos = (text) => {
-        const todoIndex = todos.findIndex((todo) => todo.text === text);
-
-        const newTodos = [...todos];
-        newTodos[todoIndex].completed = true;
-
-        saveTodos(newTodos);
-    };
-
-    const deleteTodo = (text) => {
-        const todoIndex = todos.findIndex((todo) => todo.text === text);
-
-        const newTodos = [...todos];
-        newTodos.splice(todoIndex, 1);
-
-        saveTodos(newTodos);
-    };
-
     return (
-        <AppUI
-            loading={loading}
-            error={error}
-            completedTodos={completedTodos}
-            totalTodos={totalTodos}
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            searchedTodos={searchedTodos}
-            completeTodos={completeTodos}
-            deleteTodo={deleteTodo}
-        />
+        <TodoProvider>
+            <AppUI />
+        </TodoProvider>
     );
 }
 
