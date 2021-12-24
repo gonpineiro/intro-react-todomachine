001f67f5e5f5d98c6590fca77b4d4148813c2686
Gonzalo <gonzalo@estadodigital.com>
2021-12-24 18:36:13 -0300

    high-order-components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+/* import App from './App'; */
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App(props) {
+    return (
+        <h1>
+            {props.saludo}, {props.nombre}
+        </h1>
+    );
+}
+
+function withSaludo(WrappedComponent) {
+    return function WrappedComponentWithSaludo(saludo){
+        return function ComponenteDeVerdad(props) {
+            return (
+                <>
+                    <WrappedComponent {...props} saludo={saludo}/>
+                    <p>Estamos acompañando al WrappedComponent</p>
+                </>
+            );
+        };
+    }
+}
+
+const AppWithSaludo = withSaludo(App)('wenitas');
+
+ReactDOM.render(<AppWithSaludo nombre="Gonzalo" />, document.getElementById('root'));
